cfa051174617a56b42fa68041be588d0ff7f6b78
Sharon Warner <sharon.warner56@gmail.com>
2020-04-14 09:56:26 -0400

    Adding bootstrap and react-bootstrap

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 class App extends React.Component {
     render() {
-        return <div>Hello World!</div>;
+        return <div>
+          Hello World
+          <Button variant="primary">Primary</Button>
+        </div>;
     }
 }
 
