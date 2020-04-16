0d3c53aabdc184bb1364541f5f1e7fefcab50809
Sharon Warner <sharon.warner56@gmail.com>
2020-04-16 16:26:02 -0400

    Adding react bootstrap (#33)

--- src/client/App.js
+++ src/client/App.js
@@ -1,10 +1,16 @@
 import React, { Suspense } from "react";
+import Badge from "react-bootstrap/Badge";
 import { useTranslation } from "react-i18next";
 
 function Page() {
   const { t } = useTranslation();
 
-  return <div>{t("helloWorld")}</div>;
+  return (
+    <div>
+      {t("helloWorld")}
+      <Badge>New</Badge>
+    </div>
+  );
 }
 
 export default function App() {
