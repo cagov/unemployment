af34fe92ddb40f4b741621dde14dba61b094bd9b
Kalvin Wang <kalvinwang@users.noreply.github.com>
2020-04-16 10:18:45 -0400

    Internationalize site with support for English/Spanish (#30)
    This PR adds the react-i18next and i18next-browser-languagedetector libraries, and creates public/locales/[en|es]/translation.json files for English (en) and Spanish (es), where all content can be centralized.
    
    To view Spanish, check out this branch and visit http://localhost:3000/?lng=es.

---
+++ src/client/i18n.js
@@ -0,0 +1,27 @@
+import HttpApi from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+i18n
+  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
+  // learn more: https://github.com/i18next/i18next-http-backend
+  .use(HttpApi)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init({
+    fallbackLng: "en",
+    debug: false,
+
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+    load: "languageOnly", // ignore the -US in en-US
+  });
+
+export default i18n;
