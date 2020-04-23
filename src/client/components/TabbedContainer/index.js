fcfdad595e4c92573bbd88a79336b3cc0d3ffeb5
Kalvin <82277+kalvinwang@users.noreply.github.com>
2020-04-23 16:46:52 -0400

    add title and button navigation

--- src/client/components/TabbedContainer/index.js
+++ src/client/components/TabbedContainer/index.js
@@ -1,7 +1,8 @@
+import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
-import React from "react";
+import React, { useState } from "react";
 import Row from "react-bootstrap/Row";
 import Tab from "react-bootstrap/Tab";
 import { useTranslation } from "react-i18next";
@@ -30,16 +31,32 @@ function TabbedContainer() {
     "tab6Content",
   ];
 
+  const [activeKey, setActiveKey] = useState(0);
+
+  const handleClick = (key) => {
+    setActiveKey(key);
+  };
+
   return (
     <Container>
-      <Tab.Container id="left-tabs-example" defaultActiveKey="0">
+      <Tab.Container
+        id="left-tabs-example"
+        defaultActiveKey="0"
+        activeKey={activeKey}
+      >
         <Row className="tabbed-container">
           <Col sm={4} className="TabbedContainer">
             <Nav variant="pills" className="flex-column">
               {tabTitleKeys.map((value, index) => {
                 return (
                   <Nav.Item key={index}>
-                    <Nav.Link eventKey={index}>{t(value)}</Nav.Link>
+                    <Nav.Link
+                      eventKey={index}
+                      href={"#" + value}
+                      onClick={handleClick.bind(this, index)}
+                    >
+                      {t(value)}
+                    </Nav.Link>
                   </Nav.Item>
                 );
               })}
@@ -47,10 +64,17 @@ function TabbedContainer() {
           </Col>
           <Col sm={8}>
             <Tab.Content>
-              {tabContentKeys.map((value, index) => {
+              {tabTitleKeys.map((value, index) => {
                 return (
                   <Tab.Pane eventKey={index} key={index}>
-                    {t(value)}
+                    <h2>{t(value)}</h2>
+                    <Button
+                      variant="secondary"
+                      href={"#" + tabTitleKeys[index + 1]}
+                      onClick={handleClick.bind(this, index + 1)}
+                    >
+                      {t("buttonNextPrefix") + t(tabTitleKeys[index + 1])}
+                    </Button>
                   </Tab.Pane>
                 );
               })}
