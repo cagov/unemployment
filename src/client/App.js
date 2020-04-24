faecc25066064f4af28dfb534e18007acf5040d1
Kalvin <82277+kalvinwang@users.noreply.github.com>
2020-04-24 17:42:01 -0400

    add english

--- src/client/App.js
+++ src/client/App.js
@@ -1,18 +1,16 @@
 import React, { Suspense } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-// import Subheader from "./components/Subheader";
-// import TabbedContainer from "./components/TabbedContainer";
+import Subheader from "./components/Subheader";
+import TabbedContainer from "./components/TabbedContainer";
 
 function Page() {
   return (
     <div>
       <Header />
       <main id="back-to-top">
-        {/*
         <Subheader />
         <TabbedContainer />
-      */}
       </main>
       <Footer />
     </div>
