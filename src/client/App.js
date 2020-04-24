e89d0a00d79d7586e812e06b356d128c89a31341
Sharon Warner <sharon.warner56@gmail.com>
2020-04-24 18:13:08 -0400

    Bringing back our content

--- src/client/App.js
+++ src/client/App.js
@@ -1,19 +1,16 @@
 import React, { Suspense } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-// import Subheader from "./components/Subheader";
-// import TabbedContainer from "./components/TabbedContainer";
+import Subheader from "./components/Subheader";
+import TabbedContainer from "./components/TabbedContainer";
 
 function Page() {
   return (
     <div>
       <Header />
       <main id="back-to-top">
-        {/*
         <Subheader />
-        Adding a comment to trigger a deploy
         <TabbedContainer />
-      */}
       </main>
       <Footer />
     </div>
