ccbbdc73ec95e8b4f1d555d80feec5efe4702011
Sharon Warner <sharon.warner56@gmail.com>
2020-04-24 16:27:39 -0400

    Screen 2 content!

--- src/client/components/TabPaneContent1/index.js
+++ src/client/components/TabPaneContent1/index.js
@@ -1,10 +1,61 @@
 import React from "react";
 // import { useTranslation } from "react-i18next";
 
-function TabPaneContent1() {
+function TabPaneContent0() {
   // const { t } = useTranslation();
 
-  return <div>blah</div>;
+  return (
+    <div>
+      <h4>
+        1. Evidence of citizenship or authorization to work in the United States
+      </h4>
+      <p>
+        You will need <strong>all</strong> of following:
+      </p>
+      <ul>
+        <li>Social Security number</li>
+        <li>And, California driver license or identification (ID) card</li>
+        <li>
+          And, citizenship status (this may include your Alien Registration
+          Number)
+        </li>
+      </ul>
+      <p>
+        You must be authorized to work in the US to receive UI or PUA. If you
+        are not a US citizen, have information from your employment
+        authorization document ready.
+      </p>
+      <h4>2. Information about your work history</h4>
+      <p>
+        Your employment history affects the amount of benefits you receive each
+        week. Submitting accurate information helps us process your claim
+        faster. You will need:
+      </p>
+      <ul>
+        <li>
+          <strong>Work history from the last 18 months.</strong>
+        </li>
+        <li>
+          <strong>
+            Specific information from your last employer or company.
+          </strong>
+        </li>
+      </ul>
+      <h4>Federal and military documents</h4>
+      <ul>
+        <li>
+          <strong>If you are a former federal employee,</strong> have your
+          Notice to Federal Employees About Unemployment Insurance (Standard
+          Form 8) ready.
+        </li>
+        <li>
+          <strong>If you served in the military in the last 18 months,</strong>{" "}
+          have your Certificate of Release or Discharge from Active Duty (DD
+          214) ready.
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default TabPaneContent1;
+export default TabPaneContent0;
