d34375941ec2a721513e8b86b8f67ea971645835
tony chang <tony@ponderer.org>
2020-04-27 12:40:46 -0700

    rename tests

--- src/routes/single-page-app.test.js
+++ src/routes/single-page-app.test.js
@@ -4,21 +4,21 @@ import { init } from "../app";
 const server = init();
 
 describe("Router: Single page app", () => {
-  it("renders homepage", async () => {
+  it("renders to /guide", async () => {
     const res = await request(server).get("/guide");
 
     expect(res.status).toBe(200);
     expect(res.text).toMatch(/<html/);
   });
 
-  it("returns 301 status code", async () => {
+  it("/guide/ returns 301 status code", async () => {
     const res = await request(server).get("/guide/");
 
     expect(res.status).toBe(301);
     expect(res.get('Location')).toBe('/guide');
   });
 
-  it("returns 301 status code", async () => {
+  it("/ returns 301 status code", async () => {
     const res = await request(server).get("/");
 
     expect(res.status).toBe(301);
