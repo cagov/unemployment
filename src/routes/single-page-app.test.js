c2792adcba3c6845b081e1118ea7214782ca31c9
tony chang <tony@ponderer.org>
2020-04-27 11:17:22 -0700

    Anchor the app to /guide
    Attempts to access any other path will redirect to /guide.
    
    This fixes images being broken when there is a trailing slash
    in the url.

--- src/routes/single-page-app.test.js
+++ src/routes/single-page-app.test.js
@@ -1,19 +1,27 @@
-import { init } from "../app";
 import request from "supertest";
+import { init } from "../app";
 
 const server = init();
 
 describe("Router: Single page app", () => {
   it("renders homepage", async () => {
-    const res = await request(server).get("/");
+    const res = await request(server).get("/guide");
 
     expect(res.status).toBe(200);
     expect(res.text).toMatch(/<html/);
   });
 
-  it("returns 404 status code", async () => {
-    const res = await request(server).get("/derp");
+  it("returns 301 status code", async () => {
+    const res = await request(server).get("/guide/");
 
-    expect(res.status).toBe(404);
+    expect(res.status).toBe(301);
+    expect(res.get('Location')).toBe('/guide');
+  });
+
+  it("returns 301 status code", async () => {
+    const res = await request(server).get("/");
+
+    expect(res.status).toBe(301);
+    expect(res.get('Location')).toBe('/guide');
   });
 });
