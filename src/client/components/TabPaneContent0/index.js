ac2d83a5be41866fd0819ecc9c299aab21f91e43
Sharon Warner <sharon.warner56@gmail.com>
2020-05-01 16:59:34 -0400

    Content updates

--- src/client/components/TabPaneContent0/index.js
+++ src/client/components/TabPaneContent0/index.js
@@ -23,14 +23,15 @@ function TabPaneContent0({ loadTab, tabSlugs, getTabTitle }) {
       <div className="gray-box mt-0">
         <h3>Tips for applying on UI Online</h3>
         <ul>
-          <li>
+          <li className="mb-3">
             Everyone (UI and PUA applicants) uses the same form.{" "}
             <a href="https://www.edd.ca.gov/Benefit_Programs_Online.htm">
               Register or log in with Benefit Programs Online
             </a>{" "}
             and click “File New Claim.”
+            <br />
           </li>
-          <li>
+          <li className="mb-3">
             File a new claim if you have already applied for UI but are eligible
             for PUA because you are a business owner, independent contractor,
             self-employed worker, freelancer, or gig worker affected by
@@ -38,48 +39,83 @@ function TabPaneContent0({ loadTab, tabSlugs, getTabTitle }) {
             appealed a $0 award notice, been asked to verify your identity, or
             requested a wage investigation.
           </li>
-          <li>
+          <li className="mb-3">
             If you are a small business owner, independent contractor,
             self-employed worker, freelancer, or gig worker, here’s how to
             answer key questions in the application form:
             <ul>
               <li>
-                on the Employment History screen when you supply your last
-                employer information, select <strong>No</strong>.
+                On the Employment History screen when you supply your last
+                employer information, select <strong>"No"</strong>.
               </li>
               <li>
-                on the Availability Information page, answer question 7 with{" "}
-                <strong>No</strong>.
+                On the Availability Information page, answer question 7 with{" "}
+                <strong>"No"</strong>.
               </li>
               <li>
-                on the Disaster Information page, answer question 1a.3 with “You
+                On the Disaster Information page, answer question 1a.3 with “You
                 are an independent contractor.” If you got paid in cash, select
                 “None of these options apply to me.”
               </li>
             </ul>
           </li>
+          <li className="mb-3">
+            If you have run out of benefits, you are unemployed, and your
+            benefit year has expired, reapply for benefits through UI Online.
+          </li>
+          <li className="mb-3">
+            If you have run out of benefits but you are still within the one
+            year that your claim is good for, and your benefits
+            <ul>
+              <li>
+                Ran out <strong>before February 2</strong>, you are probably not
+                eligible for PUA. (You would have to show that you are
+                unemployed because of COVID-19.) You may be eligible for the
+                federal 13-week extension. We will notify you about what to do.
+              </li>
+              <li>
+                Were for weeks you spent unemployed{" "}
+                <strong>between February 2 and March 21</strong>, we will send
+                you a form to fill out to supply information not already in your
+                claim. When you get the form, fill it out and return it.
+              </li>
+            </ul>
+          </li>
+          <li className="mb-3">
+            If you received your last payment for weeks you were unemployed{" "}
+            <strong>on or after March 21</strong> and are still within your
+            benefit year, we will automatically extend your claim for 13 weeks.
+            We will notify you that we have done that and when you can certify.
+          </li>
+          <li className="mb-3">
+            If you applied for unemployment assistance but didn’t qualify (for
+            example, because you voluntarily quit or you were fired or
+            discharged by your employer), or you are serving a penalty because
+            of false statements on a past claim, you could be eligible for PUA.
+            We will send you a form to fill out to supply information not
+            already in your account. When you get the form, fill it out and
+            return it.
+          </li>
         </ul>
       </div>
       <h2>Benefits you can apply for</h2>
       <p>
-        These benefits are available for all California residents. If you are
-        still unsure of your eligibility or what to apply for,{" "}
-        <strong>apply anyway</strong>, and as soon as you are out of work.
-        Complete the application as best you can, and we will follow up with you
-        as soon as possible.
+        These benefits are available for California residents. If you are still
+        unsure of your eligibility or what to apply for,{" "}
+        <strong>apply anyway</strong> as soon as you are out of work or had your
+        hours reduced. Complete the application as best you can, and we will
+        follow up with you as soon as possible.
       </p>
       <h3>Unemployment Insurance (UI)</h3>
       <p>
-        If you are a full-time worker at your company and only received a W-2
-        tax form last year, or you received both W-2 and 1099 tax forms last
-        year, you are most likely eligible for UI.
-      </p>
-      <p>
-        You may be eligible for regular Unemployment Insurance (UI) if you meet{" "}
-        <strong>any</strong> of the following criteria:
+        You may be eligible for regular UI if you have income reported on a W2
+        and you meet <strong>any</strong> of these:
       </p>
       <ul>
-        <li>Your employment status has been affected by COVID-19.</li>
+        <li>
+          Your employment status has been affected by COVID-19 and you are fully
+          or partially unemployed as a result.
+        </li>
         <li>
           You are fully or partially unemployed. This includes layoffs,
           furloughs, reduced wages, or reduced hours. You can still receive
@@ -93,21 +129,17 @@ function TabPaneContent0({ loadTab, tabSlugs, getTabTitle }) {
       </ul>
       <p>
         If you're already receiving UI, review <ReceiveYourBenefitsLink /> to
-        learn how your UI claim is affected by COVID-19.
+        learn how your UI claim may be affected by COVID-19.
       </p>
       <h3>Pandemic Unemployment Assistance (PUA)</h3>
       <p>
         If you are a business owner, independent contractor, self-employed
         worker, freelancer, or gig worker and only received a 1099 tax form last
-        year, you are most likely eligible for PUA. You can apply for benefits
-        starting April 28.
-      </p>
-      <p>
-        To be eligible for PUA, you must meet <strong>any</strong> of the
-        following criteria:
+        year, you are most likely eligible for PUA.
       </p>
       <h4>
-        Any criteria related to changes in employment status due to COVID-19
+        Apply for PUA if your work situation changed because of COVID-19, and
+        you meet <strong>any</strong> of these:
       </h4>
       <ul>
         <li>
@@ -121,13 +153,14 @@ function TabPaneContent0({ loadTab, tabSlugs, getTabTitle }) {
         <li>You quit your job as a direct result of COVID-19.</li>
         <li>Your workplace is closed as a direct result of COVID-19.</li>
         <li>
-          You are self-employed or work as an independent contractor (with
-          reportable income, such as a 1099) and you are unemployed, partially
-          employed, or unable to work because COVID-19 has forced you to stop
-          working.
+          You are unemployed, partially employed, or unable to work because
+          COVID-19 has forced you to stop working.
         </li>
       </ul>
-      <h4>Any criteria related to health issues due to COVID-19</h4>
+      <h4>
+        Apply for PUA if you have health issues because of COVID-19, and you
+        meet any of these:
+      </h4>
       <ul>
         <li>
           You have been diagnosed with COVID-19 or are experiencing symptoms and
@@ -139,9 +172,8 @@ function TabPaneContent0({ loadTab, tabSlugs, getTabTitle }) {
           has been diagnosed with COVID-19.
         </li>
         <li>
-          You are caring for a dependent because their school or another care
-          facility has closed due to COVID-19 and you are now caring for them
-          during the work day.
+          You are caring for a dependent during the work day because their
+          school or another care facility has closed due to COVID-19.
         </li>
         <li>
           Your healthcare provider has told you to self-quarantine because of
@@ -156,8 +188,8 @@ function TabPaneContent0({ loadTab, tabSlugs, getTabTitle }) {
       <p>
         If you’re not eligible for UI, you may be eligible for Disability
         Insurance (DI) or Paid Family Leave (PFL). You usually qualify if you
-        have a W-2 tax form, because taxes have been taken out automatically to
-        pay for these programs.
+        paid into the State Disability Insurance program (noted as “CASDI” on
+        paystubs), via taxes.
       </p>
       <ul>
         <li>
@@ -168,7 +200,7 @@ function TabPaneContent0({ loadTab, tabSlugs, getTabTitle }) {
           </a>
         </li>
         <li>
-          If you are caring for someone who is sick,{" "}
+          If you are caring for a family member who is sick,{" "}
           <a href="https://edd.ca.gov/Disability/How_to_File_a_PFL_Claim_in_SDI_Online.htm">
             apply for Paid Family Leave.
           </a>
