31aaebb9d92e46a93391189fb5d03648eab60272
Sharon Warner <sharon.warner56@gmail.com>
2020-05-01 09:21:20 -0400

    Making footer accessible for screen readers (#117)

--- src/client/components/Footer/index.js
+++ src/client/components/Footer/index.js
@@ -1,81 +1,80 @@
 import Container from "react-bootstrap/Container";
-import IcomoonReact from "icomoon-react";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import React from "react";
-import iconSet from "../../../../public/icons.json";
+import { useTranslation } from "react-i18next";
 
-class Footer extends React.PureComponent {
-  render() {
-    return (
-      <footer className="footer">
-        <Navbar className="justify-content-between" variant="custom" bg="dark">
-          <Nav>
-            <Nav.Link href="#back-to-top">Back to Top</Nav.Link>
-            <Nav.Link href="https://edd.ca.gov/about_edd/">About EDD</Nav.Link>
-            <Nav.Link href="https://edd.ca.gov/about_edd/contact_edd.htm">
-              Contact EDD
-            </Nav.Link>
-            <Nav.Link href="https://edd.ca.gov/about_edd/conditions_of_use.htm">
-              Conditions of Use
-            </Nav.Link>
-            <Nav.Link href="https://edd.ca.gov/about_edd/privacy_policy.htm">
-              Privacy Policy
-            </Nav.Link>
-            <Nav.Link href="https://edd.ca.gov/about_edd/accessibility.htm">
-              Accessibility
-            </Nav.Link>
-            <Nav.Link href="https://edd.ca.gov/sitemap.htm">Site Map</Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link href="https://www.facebook.com/californiaedd/">
-              <IcomoonReact
-                iconSet={iconSet}
-                color="#ffffff"
-                size={20}
-                icon="facebook"
-              />{" "}
-            </Nav.Link>
-            <Nav.Link href="https://twitter.com/CA_EDD">
-              <IcomoonReact
-                iconSet={iconSet}
-                color="#ffffff"
-                size={20}
-                icon="twitter"
-              />{" "}
-            </Nav.Link>
-            <Nav.Link href="https://www.linkedin.com/company/californiaedd/">
-              <IcomoonReact
-                iconSet={iconSet}
-                color="#ffffff"
-                size={20}
-                icon="linkedin2"
-              />{" "}
-            </Nav.Link>
-            <Nav.Link href="https://www.instagram.com/ca_edd/">
-              <IcomoonReact
-                iconSet={iconSet}
-                color="#ffffff"
-                size={20}
-                icon="instagram"
-              />{" "}
-            </Nav.Link>
-            <Nav.Link href="https://www.youtube.com/user/CaliforniaEDDD">
-              <IcomoonReact
-                iconSet={iconSet}
-                color="#ffffff"
-                size={20}
-                icon="youtube"
-              />{" "}
-            </Nav.Link>
-          </Nav>
-        </Navbar>
-        <div className="bg-light">
-          <Container>Copyright © 2020 State of California</Container>
-        </div>
-      </footer>
-    );
-  }
+function Footer() {
+  const { t } = useTranslation();
+
+  return (
+    <footer className="footer">
+      <Navbar className="justify-content-between" variant="custom" bg="dark">
+        <Nav>
+          <Nav.Link href="#back-to-top">Back to Top</Nav.Link>
+          <Nav.Link href="https://edd.ca.gov/about_edd/">About EDD</Nav.Link>
+          <Nav.Link href="https://edd.ca.gov/about_edd/contact_edd.htm">
+            Contact EDD
+          </Nav.Link>
+          <Nav.Link href="https://edd.ca.gov/about_edd/conditions_of_use.htm">
+            Conditions of Use
+          </Nav.Link>
+          <Nav.Link href="https://edd.ca.gov/about_edd/privacy_policy.htm">
+            Privacy Policy
+          </Nav.Link>
+          <Nav.Link href="https://edd.ca.gov/about_edd/accessibility.htm">
+            Accessibility
+          </Nav.Link>
+          <Nav.Link href="https://edd.ca.gov/sitemap.htm">Site Map</Nav.Link>
+        </Nav>
+        <Nav>
+          <Nav.Link href="https://www.facebook.com/californiaedd/">
+            <img
+              src="images/facebook.svg"
+              width="15"
+              height="15"
+              alt={t("iconAltText.facebook")}
+            />{" "}
+          </Nav.Link>
+          <Nav.Link href="https://twitter.com/CA_EDD">
+            <img
+              src="images/twitter.svg"
+              width="15"
+              height="15"
+              alt={t("iconAltText.twitter")}
+            />{" "}
+          </Nav.Link>
+          <Nav.Link href="https://www.linkedin.com/company/californiaedd/">
+            <img
+              src="images/linkedin.svg"
+              width="15"
+              height="15"
+              alt={t("iconAltText.linkedIn")}
+            />{" "}
+          </Nav.Link>
+          <Nav.Link href="https://www.instagram.com/ca_edd/">
+            <img
+              src="images/instagram.svg"
+              width="15"
+              height="15"
+              alt={t("iconAltText.instagram")}
+            />{" "}
+          </Nav.Link>
+          <Nav.Link href="https://www.youtube.com/user/CaliforniaEDDD">
+            <img
+              src="images/youtube.svg"
+              width="15"
+              height="15"
+              alt={t("iconAltText.youtube")}
+            />{" "}
+          </Nav.Link>
+        </Nav>
+      </Navbar>
+      <div className="bg-light">
+        <Container>Copyright © 2020 State of California</Container>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/client/components/Header/index.js
+++ src/client/components/Header/index.js
@@ -1,80 +1,75 @@
-import IcomoonReact from "icomoon-react";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import React from "react";
-import iconSet from "../../../../public/icons.json";
+import { useTranslation } from "react-i18next";
 
-class Header extends React.PureComponent {
-  render() {
-    return (
-      <header className="header border-bottom border-secondary">
-        <Navbar
-          className="justify-content-between"
-          variant="custom"
-          bg="primary"
-        >
-          <Navbar.Brand href="https://ca.gov">
+function Header() {
+  const { t } = useTranslation();
+
+  return (
+    <header className="header border-bottom border-secondary">
+      <Navbar className="justify-content-between" variant="custom" bg="primary">
+        <Navbar.Brand href="https://ca.gov">
+          <img
+            src="images/Ca-Gov-Logo-Gold.svg"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt={t("iconAltText.caGov")}
+          />
+        </Navbar.Brand>
+        <Nav>
+          <Nav.Link href="https://edd.ca.gov">
             <img
-              src="images/Ca-Gov-Logo-Gold.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="California gov logo"
-            />
-          </Navbar.Brand>
-          <Nav>
-            <Nav.Link href="https://edd.ca.gov">
-              <IcomoonReact
-                iconSet={iconSet}
-                color="#ffffff"
-                size={15}
-                icon="home"
-              />{" "}
-              <span>Home</span>
-            </Nav.Link>
-            <Nav.Link href="https://edd.ca.gov/login.htm">
-              <IcomoonReact
-                iconSet={iconSet}
-                color="#ffffff"
-                size={15}
-                icon="key"
-              />{" "}
-              <span>Log In</span>
-            </Nav.Link>
-          </Nav>
-        </Navbar>
-        <Navbar className="" collapseOnSelect expand="md" variant="light">
-          <Navbar.Brand href="https://edd.ca.gov">
+              src="images/home.svg"
+              width="15"
+              height="15"
+              alt={t("iconAltText.home")}
+            />{" "}
+            <span>Home</span>
+          </Nav.Link>
+          <Nav.Link href="https://edd.ca.gov/login.htm">
             <img
-              src="images/edd-logo-2-Color.svg"
-              height="50"
-              width="150"
-              className="d-inline-block align-top"
-              alt="California gov logo"
-            />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse className="justify-content-between">
-            <Nav.Link href="https://edd.ca.gov/jobs.htm">
-              <span className="text-dark">Jobs</span>
-            </Nav.Link>
-            <Nav.Link href="https://edd.ca.gov/claims.htm">
-              <span className="text-dark">Claims</span>
-            </Nav.Link>
-            <Nav.Link href="https://edd.ca.gov/employers.htm">
-              <span className="text-dark">Employers</span>
-            </Nav.Link>
-            <Nav.Link href="https://edd.ca.gov/newsroom.htm">
-              <span className="text-dark">Newsroom</span>
-            </Nav.Link>
-            <Nav.Link href="https://edd.ca.gov/serp.html?q=">
-              <span className="text-dark">Search</span>
-            </Nav.Link>
-          </Navbar.Collapse>
-        </Navbar>
-      </header>
-    );
-  }
+              src="images/key.svg"
+              width="15"
+              height="15"
+              alt={t("iconAltText.key")}
+            />{" "}
+            <span>Log In</span>
+          </Nav.Link>
+        </Nav>
+      </Navbar>
+      <Navbar className="" collapseOnSelect expand="md" variant="light">
+        <Navbar.Brand href="https://edd.ca.gov">
+          <img
+            src="images/edd-logo-2-Color.svg"
+            height="50"
+            width="150"
+            className="d-inline-block align-top"
+            alt={t("iconAltText.caGov")}
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse className="justify-content-between">
+          <Nav.Link href="https://edd.ca.gov/jobs.htm">
+            <span className="text-dark">Jobs</span>
+          </Nav.Link>
+          <Nav.Link href="https://edd.ca.gov/claims.htm">
+            <span className="text-dark">Claims</span>
+          </Nav.Link>
+          <Nav.Link href="https://edd.ca.gov/employers.htm">
+            <span className="text-dark">Employers</span>
+          </Nav.Link>
+          <Nav.Link href="https://edd.ca.gov/newsroom.htm">
+            <span className="text-dark">Newsroom</span>
+          </Nav.Link>
+          <Nav.Link href="https://edd.ca.gov/serp.html?q=">
+            <span className="text-dark">Search</span>
+          </Nav.Link>
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
+  );
 }
 
 export default Header;
