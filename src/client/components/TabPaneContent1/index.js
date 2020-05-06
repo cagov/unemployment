9198af984beae42b503b720668c40e551b055d07
Kalvin <82277+kalvinwang@users.noreply.github.com>
2020-05-06 12:12:38 -0400

    initial trans conversion

--- src/client/components/TabPaneContent1/index.js
+++ src/client/components/TabPaneContent1/index.js
@@ -1,65 +1,103 @@
+import { Trans, useTranslation } from "react-i18next";
 import React from "react";
-// import { useTranslation } from "react-i18next";
 
 function TabPaneContent1() {
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
 
   return (
     <div>
       <h3>
-        Proof of citizenship or authorization to work in the United States
+        <Trans t={t} i18nKey="tab1.header10">
+          Proof of citizenship or authorization to work in the United States
+        </Trans>
       </h3>
-      <p>
-        You will need <strong>all</strong> of following:
-      </p>
+      <Trans t={t} i18nKey="tab1.p10">
+        <p>
+          You will need <strong>all</strong> of the following:
+        </p>
+      </Trans>
       <ul>
-        <li>Social Security number</li>
-        <li>California driver license or ID card</li>
         <li>
-          Proof of US citizenship, or a green card, or a visa that allows you to
-          work in the US, or an Alien Registration Number
+          <Trans t={t} i18nKey="tab1.list10-item1">
+            Social Security number
+          </Trans>
+        </li>
+        <li>
+          <Trans t={t} i18nKey="tab1.list10-item2">
+            California driver license or ID card
+          </Trans>
+        </li>
+        <li>
+          <Trans t={t} i18nKey="tab1.list10-item3">
+            Proof of US citizenship, or a green card, or a visa that allows you
+            to work in the US, or an Alien Registration Number
+          </Trans>
         </li>
       </ul>
-      <p>
-        You must be authorized to work in the US to receive unemployment
-        benefits. If you are not a US citizen, have information from your
-        employment authorization document ready.
-      </p>
-      <h3>Information about your work history</h3>
-      <p>
-        Your work history affects the amount of benefits you receive each week.
-        Submitting accurate information helps us process your claim faster. You
-        will need:
-      </p>
+      <Trans t={t} i18nKey="tab1.p20">
+        <p>
+          You must be authorized to work in the US to receive unemployment
+          benefits. If you are not a US citizen, have information from your
+          employment authorization document ready.
+        </p>
+      </Trans>
+
+      <h3>
+        <Trans t={t} i18nKey="tab1.header20">
+          Information about your work history
+        </Trans>
+      </h3>
+      <Trans t={t} i18nKey="tab1.p30">
+        <p>
+          Your work history affects the amount of benefits you receive each
+          week. Submitting accurate information helps us process your claim
+          faster. You will need:
+        </p>
+      </Trans>
       <ul>
         <li>
-          <strong>Work history from the last 18 months.</strong> This includes
-          the name of any companies as they appear on your paycheck, the dates
-          of employment, hours worked per week, the total wages you earned
-          before taxes were taken out (gross wages), hourly rate of pay, and the
-          reason you are no longer working.
+          <Trans t={t} i18nKey="tab1.list20-item1">
+            <strong>Work history from the last 18 months.</strong> This includes
+            the name of any companies as they appear on your paycheck, the dates
+            of employment, hours worked per week, the total wages you earned
+            before taxes were taken out (gross wages), hourly rate of pay, and
+            the reason you are no longer working.
+          </Trans>
         </li>
         <li>
-          <strong>
-            Specific information from your last employer or company.
-          </strong>{" "}
-          This includes your employer’s name, mailing address, phone number,
-          supervisor’s name, total wages you earned before taxes were taken out
-          (gross wages) for the last week you worked, and the reason for your
-          unemployment.
+          <Trans t={t} i18nKey="tab1.list20-item2">
+            <strong>
+              Specific information from your last employer or company.
+            </strong>{" "}
+            This includes your employer’s name, mailing address, phone number,
+            supervisor’s name, total wages you earned before taxes were taken
+            out (gross wages) for the last week you worked, and the reason for
+            your unemployment.
+          </Trans>
         </li>
       </ul>
-      <h3>Federal and military documents</h3>
+      <h3>
+        <Trans t={t} i18nKey="tab1.header30">
+          Federal and military documents
+        </Trans>
+      </h3>
       <ul>
         <li>
-          <strong>If you are a former federal employee,</strong> have your{" "}
-          <i>Notice to Federal Employees About Unemployment Insurance</i>{" "}
-          (Standard Form 8) ready.
+          <Trans t={t} i18nKey="tab1.list30-item1">
+            <strong>If you are a former federal employee,</strong> have your{" "}
+            <i>Notice to Federal Employees About Unemployment Insurance</i>{" "}
+            (Standard Form 8) ready.
+          </Trans>
         </li>
         <li>
-          <strong>If you served in the military in the last 18 months,</strong>{" "}
-          have your <i>Certificate of Release or Discharge from Active Duty</i>{" "}
-          (DD 214) ready.
+          <Trans t={t} i18nKey="tab1.list30-item2">
+            <strong>
+              If you served in the military in the last 18 months,
+            </strong>{" "}
+            have your{" "}
+            <i>Certificate of Release or Discharge from Active Duty</i> (DD 214)
+            ready.
+          </Trans>
         </li>
       </ul>
     </div>
--- src/client/i18n.js
+++ src/client/i18n.js
@@ -16,8 +16,7 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     fallbackLng: "en",
-    debug: false,
-
+    debug: true,
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
