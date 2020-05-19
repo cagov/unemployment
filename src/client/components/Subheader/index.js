7caee9b219dd36ae60a38a366b0264978b3f7055
Sharon Warner <sharon.warner56@gmail.com>
2020-05-19 09:10:24 -0400

    WIP

--- src/client/components/Subheader/index.js
+++ src/client/components/Subheader/index.js
@@ -1,9 +1,14 @@
 import BPOButton from "../BPOButton";
 import React from "react";
+import i18n from "../../i18n";
 import { useTranslation } from "react-i18next";
 
 function Subheader() {
   const { t } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
 
   return (
     <div className="bg-light">
@@ -12,6 +17,8 @@ function Subheader() {
         <p>{t("subheaderSubheader")}</p>
         <p>{t("subheaderParagraph")}</p>
         <p>{t("subheaderParagraph2")}</p>
+        <button onClick={() => changeLanguage("es")}>es</button>
+        <button onClick={() => changeLanguage("en")}>en</button>
         <BPOButton />
       </div>
     </div>
