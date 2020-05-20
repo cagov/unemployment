b330fca8e1920543b316ad04cf9b4010f464c73b
Sharon Warner <sharon.warner56@gmail.com>
2020-05-20 09:50:03 -0400

    Language selector toggle (#205)

--- src/client/components/Subheader/index.js
+++ src/client/components/Subheader/index.js
@@ -1,9 +1,15 @@
 import BPOButton from "../BPOButton";
+import Dropdown from "react-bootstrap/Dropdown";
 import React from "react";
+import i18n from "../../i18n";
 import { useTranslation } from "react-i18next";
 
 function Subheader() {
   const { t } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
 
   return (
     <div className="bg-light">
@@ -12,7 +18,28 @@ function Subheader() {
         <p>{t("subheaderSubheader")}</p>
         <p>{t("subheaderParagraph")}</p>
         <p>{t("subheaderParagraph2")}</p>
-        <BPOButton />
+        <div className="btn-toolbar" role="toolbar">
+          <div className="mr-4 mb-4">
+            <BPOButton />
+          </div>
+          <Dropdown>
+            <Dropdown.Toggle
+              variant="outline-secondary"
+              className="text-dark bg-light"
+            >
+              Select Language
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={() => changeLanguage("en")}>
+                English
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => changeLanguage("es")}>
+                Español
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
       </div>
     </div>
   );
