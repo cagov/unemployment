c280969590ab084b05bbf305860b7de255aad969
tony chang <tony@ponderer.org>
2020-05-26 13:49:40 -0700

    Move the guide into a GuidePage component.
    This is the first step in preparing our route handling
    for having multiple pages for the retro-certs app. This
    moves the page for the guide into a separate file and
    creates the directory structure for individual pages.
    
    Also add a snapshot test.

--- src/client/App.js
+++ src/client/App.js
@@ -1,27 +1,10 @@
 import React, { Suspense } from "react";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Subheader from "./components/Subheader";
-import TabbedContainer from "./components/TabbedContainer";
-
-function Page() {
-  return (
-    <div id="overflow-wrapper">
-      <Header />
-      {/* <main> triggers a React console warning in IE11, but only in development */}
-      <main id="back-to-top">
-        <Subheader />
-        <TabbedContainer />
-      </main>
-      <Footer />
-    </div>
-  );
-}
+import GuidePage from "./pages/GuidePage";
 
 export default function App() {
   return (
     <Suspense fallback="Loading...">
-      <Page />
+      <GuidePage />
     </Suspense>
   );
 }
