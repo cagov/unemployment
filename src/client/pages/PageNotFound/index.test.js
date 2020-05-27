b749f16e931564e7c07199e0cc261505b9a9df8b
tony chang <tony@ponderer.org>
2020-05-27 14:26:43 -0700

    Add a 404 page.
    This is an error page we will show if the user
    tries to load a path without a route.
    
    I grabbed the text from the existing edd.ca.gov site.
    Note that for now the Spanish translation matches the
    English translation. The edd.ca.gov doesn't have a
    Spanish 404 page, so at least we're consistent. When
    we're getting Spanish strings in the future, we should
    include the 404 page strings.

---
+++ src/client/pages/PageNotFound/index.test.js
@@ -0,0 +1,10 @@
+import renderNonTransContent from "../../test-helpers/renderNonTransContent";
+import Component from "./index";
+
+describe("<PageNotFound />", () => {
+  it("renders a 404 page", async () => {
+    const wrapper = renderNonTransContent(Component, "PageNotFound");
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
