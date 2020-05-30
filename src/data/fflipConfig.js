ff656dc6fd322b9020d804589b8348b66075ce35
tony chang <tony@ponderer.org>
2020-05-29 17:29:26 -0700

    Add fflip for feature flags.
    This just adds the package and creates the
    config file for the retro-certs package.

---
+++ src/data/fflipConfig.js
@@ -0,0 +1,14 @@
+/**
+ * Feature and experiment flags.
+ * https://github.com/FredKSchott/fflip
+ */
+const fflipConfig = {
+  features: [{
+    id: "retroCerts",
+    description: "Allows users to retroactively certify.",
+    enabled: false
+  }],
+  criteria: []
+};
+
+module.exports = fflipConfig;
