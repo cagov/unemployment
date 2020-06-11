4bc3b8052a4e1196e98936cd6f2739c8595e325c
Sharon Warner <sharon.warner56@gmail.com>
2020-06-11 16:44:56 -0400

    WIP - recaptcha

--- src/routes/retro-certs.js
+++ src/routes/retro-certs.js
@@ -1,27 +1,30 @@
 const { Router } = require("express");
 const testAccounts = require("../data/test-accounts.json");
 const AUTH_STRINGS = require("../data/authStrings");
+const ReCaptcha = require("../services/reCaptcha");
 
 function createRouter() {
   const router = Router();
 
-  function authStatus(postJson, responseJson) {
+  async function authStatus(postJson, responseJson) {
     responseJson.status = AUTH_STRINGS.statusCode.userNotFound;
+
+    const reCaptcha = new ReCaptcha(postJson.reCaptcha);
+    const reCaptchaResponse = await reCaptcha.validateUser();
+
+    if (!reCaptchaResponse) {
+      return responseJson;
+    }
+
     for (const testAccount of testAccounts) {
-      if ((postJson.lastName || "").toLowerCase() === testAccount.lastName
-          && postJson.ssn === testAccount.ssn
-          && postJson.eddcan === testAccount.eddcan) {
+      if (
+        (postJson.lastName || "").toLowerCase() === testAccount.lastName &&
+        postJson.ssn === testAccount.ssn &&
+        postJson.eddcan === testAccount.eddcan
+      ) {
         responseJson.status = AUTH_STRINGS.statusCode.ok;
         responseJson.authToken = testAccount.authToken;
         responseJson.weeksToCertify = Array.from(testAccount.weeksToCertify);
-        break;
-      } else if (postJson.ssn === testAccount.ssn
-          && postJson.eddcan !== testAccount.eddcan) {
-        responseJson.status = AUTH_STRINGS.statusCode.wrongEddcan;
-        break;
-      } else if (postJson.ssn !== testAccount.ssn
-          && postJson.eddcan === testAccount.eddcan) {
-        responseJson.status = AUTH_STRINGS.statusCode.wrongSsn;
         break;
       }
     }
@@ -42,16 +45,18 @@ function createRouter() {
     return responseJson;
   }
 
-  router.post(AUTH_STRINGS.apiPath.login, (req, res) => {
-    const responseJson = authStatus(req.body, {});
-    const httpStatus = responseJson.status === AUTH_STRINGS.statusCode.ok ? 200 : 401
+  router.post(AUTH_STRINGS.apiPath.login, async (req, res) => {
+    const responseJson = await authStatus(req.body, {});
+    const httpStatus =
+      responseJson.status === AUTH_STRINGS.statusCode.ok ? 200 : 401;
 
     res.status(httpStatus).type("json").send(JSON.stringify(responseJson));
   });
 
   router.post(AUTH_STRINGS.apiPath.data, (req, res) => {
     const responseJson = authWithToken(req.body, {});
-    const httpStatus = responseJson.status === AUTH_STRINGS.statusCode.ok ? 200 : 401
+    const httpStatus =
+      responseJson.status === AUTH_STRINGS.statusCode.ok ? 200 : 401;
 
     res.status(httpStatus).type("json").send(JSON.stringify(responseJson));
   });
