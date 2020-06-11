c5713c6506cbcbef969ba19b5b01f72fe8eea8dd
tony chang <tony.chang@digital.ca.gov>
2020-06-10 18:06:19 -0700

    Connect the API to the staging DB.
    Remove test-accounts.js and connect to the Cosmos DB!
    The first two accounts exist in the staging DB so
    the same logins should work. Note that if multiple
    people use the same login, it will invalidate the
    first user's token!
    
    Also remove the last name from the "What to expect"
    page and add an error code for invalid recaptcha. That
    check takes precedence and will show whether your auth
    data is correct or not.
    
    Update the tests so we don't try to connect to the
    network except for the single ReCaptcha test.

--- src/routes/retro-certs.js
+++ src/routes/retro-certs.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
-const testAccounts = require("../data/test-accounts.json");
 const AUTH_STRINGS = require("../data/authStrings");
 const ReCaptcha = require("../services/reCaptcha");
+const cosmos = require("../data/cosmos");
 
 function createRouter() {
   const router = Router();
@@ -10,58 +10,65 @@ function createRouter() {
     responseJson.status = AUTH_STRINGS.statusCode.userNotFound;
 
     const reCaptcha = new ReCaptcha(postJson.reCaptcha);
-    const reCaptchaResponse = await reCaptcha.validateUser();
+
+    const [reCaptchaResponse, record] = await Promise.all([
+      reCaptcha.validateUser(),
+      cosmos.getUserByNameEddcanSsn(
+        postJson.lastName || "",
+        postJson.eddcan,
+        postJson.ssn
+      ),
+    ]);
 
     if (!reCaptchaResponse) {
+      responseJson.status = AUTH_STRINGS.statusCode.recaptchaInvalid;
       return responseJson;
     }
 
-    for (const testAccount of testAccounts) {
-      if (
-        (postJson.lastName || "").toLowerCase() ===
-          testAccount.lastName.toLowerCase() &&
-        postJson.ssn === testAccount.ssn &&
-        postJson.eddcan === testAccount.eddcan
-      ) {
-        responseJson.status = AUTH_STRINGS.statusCode.ok;
-        responseJson.authToken = testAccount.authToken;
-        responseJson.lastName = testAccount.lastName;
-        responseJson.weeksToCertify = Array.from(testAccount.weeksToCertify);
-        break;
-      }
+    if (record) {
+      responseJson.status = AUTH_STRINGS.statusCode.ok;
+      responseJson.authToken = await cosmos.createAuthTokenForUser(record.id);
+      responseJson.weeksToCertify = Array.from(record.weeksToCertify);
     }
     return responseJson;
   }
 
-  function authWithToken(postJson, responseJson) {
+  async function authWithToken(postJson, responseJson) {
     responseJson.status = AUTH_STRINGS.statusCode.userNotFound;
     if (postJson.authToken) {
-      for (const testAccount of testAccounts) {
-        if (postJson.authToken === testAccount.authToken) {
-          responseJson.status = AUTH_STRINGS.statusCode.ok;
-          responseJson.lastName = testAccount.lastName;
-          responseJson.weeksToCertify = Array.from(testAccount.weeksToCertify);
-          break;
-        }
+      const record = await cosmos.getUserByAuthToken(postJson.authToken);
+      if (record) {
+        responseJson.status = AUTH_STRINGS.statusCode.ok;
+        responseJson.weeksToCertify = Array.from(record.weeksToCertify);
       }
     }
     return responseJson;
   }
 
   router.post(AUTH_STRINGS.apiPath.login, async (req, res) => {
-    const responseJson = await authStatus(req.body, {});
-    const httpStatus =
-      responseJson.status === AUTH_STRINGS.statusCode.ok ? 200 : 401;
+    try {
+      const responseJson = await authStatus(req.body, {});
+      const httpStatus =
+        responseJson.status === AUTH_STRINGS.statusCode.ok ? 200 : 401;
 
-    res.status(httpStatus).type("json").send(JSON.stringify(responseJson));
+      res.status(httpStatus).type("json").send(JSON.stringify(responseJson));
+    } catch (e) {
+      console.error("Error during /api/login", e);
+      res.status(500).send();
+    }
   });
 
-  router.post(AUTH_STRINGS.apiPath.data, (req, res) => {
-    const responseJson = authWithToken(req.body, {});
-    const httpStatus =
-      responseJson.status === AUTH_STRINGS.statusCode.ok ? 200 : 401;
+  router.post(AUTH_STRINGS.apiPath.data, async (req, res) => {
+    try {
+      const responseJson = await authWithToken(req.body, {});
+      const httpStatus =
+        responseJson.status === AUTH_STRINGS.statusCode.ok ? 200 : 401;
 
-    res.status(httpStatus).type("json").send(JSON.stringify(responseJson));
+      res.status(httpStatus).type("json").send(JSON.stringify(responseJson));
+    } catch (e) {
+      console.error("Error during /api/data", e);
+      res.status(500).send();
+    }
   });
 
   return router;
