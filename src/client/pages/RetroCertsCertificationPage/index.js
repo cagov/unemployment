105008da3c626c190cafbab79154e2c8ecf07283
tony chang <tony.chang@digital.ca.gov>
2020-06-16 19:07:47 -0700

    Add Yes/No certification questions to the page.
    We update the userData.formData array with the
    answers provided. Also add some logic to scroll
    to the top of the form when navigating between
    weeks.
    
    Still need to implement
    - Q1 and Q6 expanders.
    - Validation.
    - Submitting the data.

--- src/client/pages/RetroCertsCertificationPage/index.js
+++ src/client/pages/RetroCertsCertificationPage/index.js
@@ -1,36 +1,66 @@
 import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import Form from "react-bootstrap/Form";
 import { Redirect, useHistory, Link } from "react-router-dom";
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useRef } from "react";
 import { useTranslation, Trans } from "react-i18next";
 import { userDataPropType, setUserDataPropType } from "../../commonPropTypes";
 import {
   fromIndexToPathString,
   fromPathStringToIndex,
+  toWeekString,
 } from "../../../utils/retroCertsWeeks";
 import mockedFormData from "../../../data/mockedFormData";
 import routes from "../../../data/routes";
 import AUTH_STRINGS from "../../../data/authStrings";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import YesNoQuestion from "../../components/YesNoQuestion";
 
 function RetroCertsCertificationPage(props) {
   const { t } = useTranslation();
   const history = useHistory();
 
   const { userData, setUserData, routeComputedMatch } = props;
-  const week = routeComputedMatch.params.week || "";
-
   const numberOfWeeks = userData.weeksToCertify.length;
+  const week = routeComputedMatch.params.week || "";
   const weekIndex = fromPathStringToIndex(week || "");
+  const headingElement = useRef(null);
+  const weekIndexRef = useRef(weekIndex);
+
   const weekForUser = userData.weeksToCertify.indexOf(weekIndex) + 1;
   if (!weekForUser) {
     // The week from the URL is not a week that the user has
     // to certify for. Send them back to the list of weeks page.
     return <Redirect to={routes.retroCertsWeeksToCertify} />;
   }
+
+  // When the user transitions to a new week, return to the top
+  // of the form.
+  if (weekIndexRef.current !== weekIndex) {
+    weekIndexRef.current = weekIndex;
+    window.scroll({
+      top: headingElement.current.offsetTop,
+      left: 0,
+      behavior: "smooth",
+    });
+  }
+
+  const formDataArray = userData.formData || [];
+  const formData = formDataArray[weekForUser - 1] || { weekIndex };
+
+  const handleYesNoChange = (event) => {
+    const { value, name } = event;
+    formDataArray[weekForUser - 1] = {
+      ...formData,
+      [name]: value,
+    };
+    setUserData({
+      ...userData,
+      formData: [...formDataArray],
+    });
+  };
 
   function handleSubmit() {
     fetch(AUTH_STRINGS.apiPath.save, {
@@ -45,7 +75,11 @@ function RetroCertsCertificationPage(props) {
     })
       .then((response) => response.json())
       .then((data) => {
-        setUserData(data);
+        setUserData({
+          ...userData,
+          confirmationNumber: data.confirmationNumber,
+        });
+
         history.push(routes.retroCertsConfirmation);
       })
       .catch((error) => console.error(error));
@@ -56,7 +90,7 @@ function RetroCertsCertificationPage(props) {
       <Header />
       <main>
         <div className="container p-4">
-          <h1>{t("retrocerts-certification.title")}</h1>
+          <h1 ref={headingElement}>{t("retrocerts-certification.title")}</h1>
           {numberOfWeeks > 1 && (
             <p>
               <Trans
@@ -66,54 +100,79 @@ function RetroCertsCertificationPage(props) {
               />
             </p>
           )}
-          <Form onSubmit={handleSubmit}>
-            <Row>Questions go here.</Row>
-            <Row>
-              <div className="col-md-4">
+          <h3>
+            <Trans
+              t={t}
+              i18nKey="retrocerts-certification.form-header"
+              values={{ weekForUser, weekString: toWeekString(weekIndex) }}
+            />
+          </h3>
+          {[
+            "tooSick",
+            "fullTime",
+            "didYouLook",
+            "refuseWork",
+            "schoolOrTraining",
+            "workOrEarn",
+          ].map((name, index) => (
+            <Row key={`${weekIndex}-${index}`}>
+              <Col>
+                <YesNoQuestion
+                  questionNumber={index + 1}
+                  questionText={t(`retrocerts-certification.q-${name}`)}
+                  helpText={t(`retrocerts-certification.qhelp-${name}`)}
+                  ifYes={formData[name]}
+                  onChange={(e) => handleYesNoChange(e)}
+                  inputName={name}
+                />
+              </Col>
+            </Row>
+          ))}
+          <Row>
+            <Col>
+              <Button
+                variant="outline-secondary"
+                className="text-dark bg-light"
+                as={Link}
+                to={
+                  weekForUser === 1
+                    ? routes.retroCertsWeeksToCertify
+                    : routes.retroCertsCertify +
+                      "/" +
+                      fromIndexToPathString(
+                        userData.weeksToCertify[weekForUser - 2]
+                      )
+                }
+              >
+                {t("retrocerts-certification.button-back")}
+              </Button>
+            </Col>
+            <Col style={{ textAlign: "end" }}>
+              {weekForUser === numberOfWeeks && (
+                <Button variant="secondary" onClick={handleSubmit}>
+                  {t("retrocerts-certification.button-submit")}
+                </Button>
+              )}
+              {weekForUser !== numberOfWeeks && (
                 <Button
-                  variant="outline-secondary"
-                  className="text-dark bg-light"
+                  variant="secondary"
                   as={Link}
+                  type="submit"
                   to={
-                    weekForUser === 1
-                      ? routes.retroCertsWeeksToCertify
-                      : routes.retroCertsCertify +
-                        "/" +
-                        fromIndexToPathString(
-                          userData.weeksToCertify[weekForUser - 2]
-                        )
+                    routes.retroCertsCertify +
+                    "/" +
+                    fromIndexToPathString(userData.weeksToCertify[weekForUser])
                   }
                 >
-                  {t("retrocerts-certification.button-back")}
+                  <Trans
+                    t={t}
+                    i18nKey="retrocerts-certification.button-next"
+                    values={{ nextWeekForUser: weekForUser + 1 }}
+                  />
                 </Button>
-              </div>
-              <div className="col-md-4">
-                {weekForUser === numberOfWeeks && (
-                  <Button variant="secondary" onClick={handleSubmit}>
-                    {t("retrocerts-certification.button-submit")}
-                  </Button>
-                )}
-                {weekForUser !== numberOfWeeks && (
-                  <Button
-                    variant="secondary"
-                    as={Link}
-                    type="submit"
-                    to={
-                      routes.retroCertsCertify +
-                      "/" +
-                      fromIndexToPathString(weekForUser)
-                    }
-                  >
-                    <Trans
-                      t={t}
-                      i18nKey="retrocerts-certification.button-next"
-                      values={{ nextWeekForUser: weekForUser + 1 }}
-                    />
-                  </Button>
-                )}
-              </div>
-            </Row>
-          </Form>
+              )}
+            </Col>
+          </Row>
         </div>
       </main>
       <Footer />
