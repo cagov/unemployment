1d24969ae20a9991ff5128ce56daa9e8b61528a5
tony chang <tony.chang@digital.ca.gov>
2020-06-22 14:45:36 -0700

    Update the weeks to certify and confirmation page
    * Remove the what to expect page and update the routing.
    * List the weeks to certify on the weeks to certify page.
    * Create a component for listing weeks.
    * Update the confirmation page
      * Remove the confirmation number
      * Update text to handle 2 cases (returning user or not).
      * Use the list of weeks component.

--- src/utils/retroCertsWeeks.js
+++ src/utils/retroCertsWeeks.js
@@ -1,41 +1,44 @@
 const START_DATE = [
-  "02/02/20",
-  "02/09/20",
-  "02/16/20",
-  "02/23/20",
-  "03/01/20",
-  "03/08/20",
-  "03/15/20",
-  "03/22/20",
-  "03/29/20",
-  "04/05/20",
-  "04/12/20",
-  "04/19/20",
-  "04/26/20",
-  "05/03/20",
+  "02/02/2020",
+  "02/09/2020",
+  "02/16/2020",
+  "02/23/2020",
+  "03/01/2020",
+  "03/08/2020",
+  "03/15/2020",
+  "03/22/2020",
+  "03/29/2020",
+  "04/05/2020",
+  "04/12/2020",
+  "04/19/2020",
+  "04/26/2020",
+  "05/03/2020",
 ];
 
 const END_DATE = [
-  "02/08/20",
-  "02/15/20",
-  "02/22/20",
-  "02/29/20",
-  "03/07/20",
-  "03/14/20",
-  "03/21/20",
-  "03/28/20",
-  "04/04/20",
-  "04/11/20",
-  "04/18/20",
-  "04/25/20",
-  "05/02/20",
-  "05/09/20",
+  "02/08/2020",
+  "02/15/2020",
+  "02/22/2020",
+  "02/29/2020",
+  "03/07/2020",
+  "03/14/2020",
+  "03/21/2020",
+  "03/28/2020",
+  "04/04/2020",
+  "04/11/2020",
+  "04/18/2020",
+  "04/25/2020",
+  "05/02/2020",
+  "05/09/2020",
 ];
+
+const startAndEndDate = (index) => {
+  return { startDate: START_DATE[index], endDate: END_DATE[index] };
+};
 
 const toWeekString = (index) => {
   return START_DATE[index] + " - " + END_DATE[index];
 };
-
 /**
  * Converts the date from YYYY-MM-DD to an index (0-13).
  * The date should be the Saturday of the week.
@@ -43,12 +46,12 @@ const toWeekString = (index) => {
  * @returns {number} From 0-13 or -1 if it's not a valid date.
  */
 const fromPathStringToIndex = (dateString) => {
-  const matches = dateString.match(/20(\d\d)-(\d\d)-(\d\d)/);
+  const matches = dateString.match(/(20\d\d)-(\d\d)-(\d\d)/);
   if (!matches || matches.length !== 4) {
     return -1;
   }
-  const [shortYear, month, day] = matches.slice(1);
-  const endDateString = month + "/" + day + "/" + shortYear;
+  const [year, month, day] = matches.slice(1);
+  const endDateString = month + "/" + day + "/" + year;
   return END_DATE.indexOf(endDateString);
 };
 
@@ -62,8 +65,13 @@ const fromIndexToPathString = (index) => {
   if (!dateString) {
     return "";
   }
-  const [month, day, shortYear] = dateString.split("/");
-  return "20" + shortYear + "-" + month + "-" + day;
+  const [month, day, year] = dateString.split("/");
+  return year + "-" + month + "-" + day;
 };
 
-module.exports = { toWeekString, fromPathStringToIndex, fromIndexToPathString };
+module.exports = {
+  startAndEndDate,
+  toWeekString,
+  fromPathStringToIndex,
+  fromIndexToPathString,
+};
