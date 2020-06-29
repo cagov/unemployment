78228c4123014df575582e369f9642bb695110f3
Kalvin <82277+kalvinwang@users.noreply.github.com>
2020-06-29 12:55:00 -0400

    generate both files

--- scripts/create_test_forms.py
+++ scripts/create_test_forms.py
@@ -3,24 +3,40 @@
 '''
 
 import json
+import csv
 import uuid
 
 NUM_TEST_FORMS = 100
 
 def get_uuid():
-  return 'test' + uuid.uuid4()
+  return 'test' + str(uuid.uuid4())
 
-def generate_test_forms(num_test_forms):
-  forms = []
+def generate_test_form_data(num_test_forms):
+  uuids = []
   for i in range(num_test_forms):
+    uuids.append(get_uuid())
+  return uuids
+
+def generate_test_form_json(test_form_data):
+  forms = []
+  for uuid in test_form_data:
       # 1. id must be unique because the forms container partitions on it (/id)
       # 2. authToken must be unique because our code looks for a unique authtoken
       forms.append({
-          'id': get_uuid(),
-          'authToken': get_uuid(),
+          'id': uuid,
+          'authToken': uuid,
           'formData': "test"
       })
   with open('test_forms.json', 'w') as outfile:
       outfile.write(json.dumps(sorted(forms, key=lambda d: d['id'])))
 
-generate_test_forms(NUM_TEST_FORMS)
+def generate_test_form_csv(test_form_data):
+  with open('test_forms.csv', 'w') as outfile:
+      writer = csv.writer(outfile, delimiter=',', lineterminator='\n')
+      writer.writerow(['authToken'])
+      for uuid in test_form_data:
+        writer.writerow([uuid])
+
+data = generate_test_form_data(NUM_TEST_FORMS)
+generate_test_form_json(data)
+generate_test_form_csv(data)
