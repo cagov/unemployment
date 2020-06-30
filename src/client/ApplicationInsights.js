5b8bb07d6ec3bb2ad76987394f9dbfb7b9112588
Kalvin <82277+kalvinwang@users.noreply.github.com>
2020-06-30 10:57:12 -0400

    add insights code

---
+++ src/client/ApplicationInsights.js
@@ -0,0 +1,46 @@
+import React, { Component, Fragment } from "react";
+import { withAITracking } from "@microsoft/applicationinsights-react-js";
+import { ai } from "../services/azureApplicationInsights";
+import { withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+
+/**
+ * This Component provides telemetry with Azure App Insights
+ *
+ * NOTE: the package '@microsoft/applicationinsights-react-js' has a HOC withAITracking that requires this to be a Class Component rather than a Functional Component
+ */
+class ApplicationInsights extends Component {
+  state = {
+    initialized: false,
+  };
+
+  componentDidMount() {
+    const { history } = this.props;
+    const { initialized } = this.state;
+    const AppInsightsInstrumentationKey = this.props.instrumentationKey; // PUT YOUR KEY HERE
+    if (
+      !initialized &&
+      Boolean(AppInsightsInstrumentationKey) &&
+      Boolean(history)
+    ) {
+      ai.initialize(AppInsightsInstrumentationKey, history);
+      this.setState({ initialized: true });
+    }
+
+    this.props.after();
+  }
+
+  render() {
+    const { children } = this.props;
+    return <Fragment>{children}</Fragment>;
+  }
+}
+
+ApplicationInsights.propTypes = {
+  history: PropTypes.object,
+  after: PropTypes.func,
+  children: PropTypes.object,
+  instrumentationKey: PropTypes.string,
+};
+
+export default withRouter(withAITracking(ai.reactPlugin, ApplicationInsights));
--- src/client/index.js
+++ src/client/index.js
@@ -6,6 +6,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import { render } from "react-dom";
+import ApplicationInsights from "./ApplicationInsights";
 import smoothscroll from "smoothscroll-polyfill";
 
 // Enables scrolling (not just smooth scrolling) on Edge and IE
@@ -13,7 +14,12 @@ smoothscroll.polyfill();
 
 render(
   <BrowserRouter>
-    <App />
+    <ApplicationInsights
+      instrumentationKey="57ab6c1e-220e-47fa-96ab-fe49b6a9dc73"
+      after={() => {}}
+    >
+      <App />
+    </ApplicationInsights>
   </BrowserRouter>,
   document.getElementById("root")
 );
