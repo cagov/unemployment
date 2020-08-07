a2ed12c5bb8ce0cdd7616938864de144cb2ab5a4
Kalvin Wang <kalvinwang@users.noreply.github.com>
2020-08-07 16:47:16 -0400

    Enable submit on Staff View auth page (#539)
    This PR connects the Staff View auth page to the new POST request, and updates the Staff View confirmation page to use actual data. Staff View works!

--- src/client/pages/StaffViewConfirmationPage/index.test.js
+++ src/client/pages/StaffViewConfirmationPage/index.test.js
@@ -1,59 +1,59 @@
-import renderNonTransContent from "../../test-helpers/renderNonTransContent";
+import renderTransContent from "../../test-helpers/renderTransContent";
 import Component from "./index";
 import AUTH_STRINGS from "../../../data/authStrings";
 
 describe("<StaffViewConfirmationPage />", () => {
   it("when claimant not started", async () => {
-    const wrapper = renderNonTransContent(
-      Component,
-      "StaffViewConfirmationPage",
-      {
-        userData: {
-          hasLoggedIn: false,
-          status: AUTH_STRINGS.statusCode.ok,
-          weeksToCertify: [0, 1, 2, 5, 6],
-          confirmationNumber: "CONFIRMATION_NUMBER",
-          programPlan: ["PUA full time"],
-        },
-        setUserData: () => {},
-      }
-    );
+    const wrapper = renderTransContent(Component, {
+      userData: {
+        status: AUTH_STRINGS.statusCode.ok,
+        weeksToCertify: [0, 1, 2, 5, 6],
+        programPlan: ["PUA full time"],
+        lastName: "Taylor",
+      },
+      setUserData: () => {},
+    });
 
     expect(wrapper).toMatchSnapshot();
   });
 
   it("when claimant in progress", async () => {
-    const wrapper = renderNonTransContent(
-      Component,
-      "StaffViewConfirmationPage",
-      {
-        userData: {
-          hasLoggedIn: true,
-          status: AUTH_STRINGS.statusCode.ok,
-          weeksToCertify: [0, 1, 2, 5, 6],
-          programPlan: ["UI full time"],
-        },
-        setUserData: () => {},
-      }
-    );
+    const wrapper = renderTransContent(Component, {
+      userData: {
+        authToken: "pretendThisIsARealToken",
+        status: AUTH_STRINGS.statusCode.ok,
+        weeksToCertify: [0, 1, 2, 5, 6],
+        programPlan: ["UI full time"],
+        lastName: "Taylor",
+      },
+      setUserData: () => {},
+    });
 
     expect(wrapper).toMatchSnapshot();
   });
 
   it("when claimant completed", async () => {
-    const wrapper = renderNonTransContent(
-      Component,
-      "StaffViewConfirmationPage",
-      {
-        userData: {
-          confirmationNumber: "33a6f278-eaa3-41c5-8d08-276fa0364833",
-          status: AUTH_STRINGS.statusCode.ok,
-          weeksToCertify: [0, 1, 2, 5, 6],
-          programPlan: ["UI full time"],
-        },
-        setUserData: () => {},
-      }
-    );
+    const weekOfAnswers = {
+      tooSick: false,
+      couldNotAcceptWork: false,
+      didYouLook: false,
+      refuseWork: false,
+      schoolOrTraining: false,
+      workOrEarn: false,
+    };
+
+    const wrapper = renderTransContent(Component, {
+      userData: {
+        authToken: "pretendThisIsARealToken",
+        confirmationNumber: "33a6f278-eaa3-41c5-8d08-276fa0364833",
+        status: AUTH_STRINGS.statusCode.ok,
+        weeksToCertify: [0, 1, 2],
+        programPlan: ["UI full time"],
+        lastName: "Taylor",
+        formData: [weekOfAnswers, weekOfAnswers, weekOfAnswers],
+      },
+      setUserData: () => {},
+    });
 
     expect(wrapper).toMatchSnapshot();
   });
