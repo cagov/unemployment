1b695116be067a3560f7001e2e3f31776aaa04c5
Sharon Warner <sharon.warner56@gmail.com>
2020-09-04 12:27:38 -0400

    Removing retrocert feature toggle

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,11 @@
 const { Router } = require("express");
 const singlePageApp = require("./single-page-app");
 const createRetroCertsRouter = require("./retro-certs");
-const fflip = require("fflip");
 
 function createRouter() {
   const router = Router();
 
-  if (fflip.features.retroCerts.enabled) {
-    router.use(createRetroCertsRouter());
-  }
+  router.use(createRetroCertsRouter());
 
   /**
    * Frontend web page router. This uses a wildcard, so we place it
