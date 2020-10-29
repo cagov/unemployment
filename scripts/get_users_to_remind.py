c2091d0252f17ee805ca355b420bf25b119fce38
Kalvin <82277+kalvinwang@users.noreply.github.com>
2020-10-29 14:15:46 -0400

    update filename

--- scripts/get_users_to_remind.py
+++ scripts/get_users_to_remind.py
@@ -4,17 +4,19 @@
 
 from users import *
 import json
+from datetime import date
 
 with open('/scripts/forms.json') as json_file:
     data = json.load(json_file)
 
 userIdsOnCompletedForms = [i['id'] for i in data]
-
 incompleteUsers = list(set(userIds) - set(userIdsOnCompletedForms))
 
 print(len(userIds));
 print(len(userIdsOnCompletedForms));
 print(len(incompleteUsers));
 
-with open('result.txt', 'w') as file:
+outputFilename = ('hashesForUsersWhoHaveNotCompletedTheForm-{}.txt'
+    .format(date.today().strftime("%Y%m%d")))
+with open(outputFilename, 'w') as file:
     file.writelines("%s\n" % user for user in incompleteUsers)
