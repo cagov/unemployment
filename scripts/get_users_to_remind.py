ef9afc556aa577c272e6e8fc21051b5ce894c780
tony chang <tony.chang@digital.ca.gov>
2020-11-16 15:35:00 -0800

    Update script to read users from a json file.
    Since I don't have a users.py file.

--- scripts/get_users_to_remind.py
+++ scripts/get_users_to_remind.py
@@ -1,20 +1,23 @@
 # The two inputs for this script are intentionally not shared
-# The users.py file is constant between runs and contains the hashes for all users
+# The users.json file is constant between runs and contains the hashes for all users
 # The forms.json file is exported directly from CosmosDB using the Data Migration Tool
 
-from users import *
 import json
 from datetime import date
 
-with open('/scripts/forms.json') as json_file:
+with open('users.json') as json_file:
+    userJson = json.load(json_file)
+    userIds = set(u['id'] for u in userJson)
+
+with open('forms.json') as json_file:
     data = json.load(json_file)
 
 userIdsOnCompletedForms = [i['id'] for i in data]
 incompleteUsers = list(set(userIds) - set(userIdsOnCompletedForms))
 
-print(len(userIds));
-print(len(userIdsOnCompletedForms));
-print(len(incompleteUsers));
+print(len(userIds))
+print(len(userIdsOnCompletedForms))
+print(len(incompleteUsers))
 
 outputFilename = ('hashesForUsersWhoHaveNotCompletedTheForm-{}.txt'
     .format(date.today().strftime("%Y%m%d")))
